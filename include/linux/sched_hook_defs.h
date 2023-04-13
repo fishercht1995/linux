919f4468a70abc59c575cb64350c9837f6363d17
fishercht1995 <45677828+fishercht1995@users.noreply.github.com>
2023-04-13 03:52:44 -0400

    Update sched_hook_defs.h

--- include/linux/sched_hook_defs.h
+++ include/linux/sched_hook_defs.h
@@ -3,3 +3,4 @@ BPF_SCHED_HOOK(int, 0, cfs_check_preempt_tick, struct sched_entity *curr, unsign
 BPF_SCHED_HOOK(int, 0, cfs_check_preempt_wakeup, struct task_struct *curr, struct task_struct *p)
 BPF_SCHED_HOOK(int, 0, cfs_wakeup_preempt_entity, struct sched_entity *curr, struct sched_entity *se)
 BPF_SCHED_HOOK(int, 0, cfs__schedule, struct task_struct *prev, unsigned long delta_exec, int check_pred)
+BPF_SCHED_HOOK(int, 0, cfs_entity_before, int apred, int bpred, u64 avmtime, u64 bvmtime)
